4aa5434f6dcd70d8b23d5f4e7939c73b8006a06f
Eric Kelly <heroiceric@gmail.com>
2013-05-08 12:51:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shmoogle::Application.config.secret_token = 'da349fca3723f01c15e944ecbf9986dc37366f819e2025c24a3c61062dcf423e0e378c60ed5b140cf5e5c6aae50ddbf6378a6f15595d4d1fb323ae182811e8c5'
